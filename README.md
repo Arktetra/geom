a2e635808587fe384ce1b59b6aa2a6a16d55df96
Arktetra <sameershrestha.sm@gmail.com>
2025-07-06 18:29:27 +0545

    add script for creating cube

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # geom
 
 Understanding Geometry.
+
+## Setup
+
+```
+pip install .
+```
+
+## Data
+
+Run the following data to generate dummy data of a cube viewed from multiple sides.
+
+```
+python python_scripts/blender/cube.py
+```
+
+The results will be stored in the `assets` directory in the root.
--- python_scripts/blender/cube.py
+++ python_scripts/blender/cube.py
@@ -1,1 +1,37 @@
+import bpy
+from pathlib import Path
 
+from geom.utils.blender import (
+    Camera, Light, Cube, remove_all_objects
+)
+
+# clean up the scene by removing all existing objects
+remove_all_objects()
+
+# create a cube with cyan material
+cube = Cube("Cube")
+cube.add_material("cyan", (0, 1, 1, 1))
+
+# add a point light to illuminate the cube
+light = Light("Point", "POINT", energy=3000)
+
+# create and position multiple cameras around the cube
+camera_locs = [(3, -3, 5), (3, 3, 5), (-3, 3, 5), (-3, -3, 5)]
+camera = Camera("Camera")
+for i, loc in enumerate(camera_locs):
+    camera.add(f"camera_{i}", loc)
+camera.look_at(cube.obj)
+
+assets_dir = Path("./assets/cube/")
+assets_dir.mkdir(parents=True, exist_ok=True)
+
+bpy.context.scene.render.resolution_x = 640
+bpy.context.scene.render.resolution_y = 480
+
+bpy.context.scene.render.image_settings.file_format = "PNG"
+bpy.context.scene.render.image_settings.color_mode = "RGBA"
+
+for i, cam_obj in enumerate(camera.objs):
+    bpy.context.scene.camera = cam_obj
+    bpy.ops.render.render()
+    bpy.data.images["Render Result"].save_render(filepath=str(assets_dir / f"{i}.png"))
